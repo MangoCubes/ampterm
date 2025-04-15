ceb3ab8c05b306b7aa4a6b55fd34346c0736c3b3
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-04-16 00:17:07 +0900

    Add loading screen component

---
+++ src/components/home/loading.rs
@@ -0,0 +1,42 @@
+use color_eyre::Result;
+use ratatui::{
+    layout::{Constraint, Flex, Layout, Rect},
+    text::Line,
+    widgets::{Block, Paragraph},
+    Frame,
+};
+
+use crate::components::Component;
+
+pub struct Loading {
+    url: String,
+    username: String,
+}
+
+impl Loading {
+    pub fn new(url: String, username: String) -> Self {
+        Self { url, username }
+    }
+}
+
+impl Component for Loading {
+    fn draw(&mut self, frame: &mut Frame, area: Rect) -> Result<()> {
+        let [horizontal] = Layout::horizontal([Constraint::Percentage(50)])
+            .flex(Flex::Center)
+            .areas(area);
+        let [centered] = Layout::vertical([Constraint::Percentage(50)])
+            .flex(Flex::Center)
+            .areas(horizontal);
+        frame.render_widget(
+            Paragraph::new(vec![
+                Line::raw("Logging in with the credentials in the configuration..."),
+                Line::raw(format!("URL: {}", self.url)),
+                Line::raw(format!("Username: {}", self.username)),
+            ])
+            .centered()
+            .block(Block::bordered()),
+            centered,
+        );
+        Ok(())
+    }
+}

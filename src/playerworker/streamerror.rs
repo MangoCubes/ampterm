12ba0d16d4c28f20db5bc782747ecd28a4b7bdb7
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-05-09 01:57:28 +0900

    Add stream error type

---
+++ src/playerworker/streamerror.rs
@@ -0,0 +1,55 @@
+use std::{error::Error, fmt::Display};
+use stream_download::{
+    http::{HttpStream, HttpStreamError},
+    StreamInitializationError,
+};
+
+#[derive(Debug)]
+enum ErrType {
+    Parse(String),
+    Stream(HttpStreamError<reqwest::Client>),
+    StreamInit(StreamInitializationError<HttpStream<reqwest::Client>>),
+}
+impl Display for ErrType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ErrType::Stream(e) => write!(f, "Stream Error: {}", e),
+            ErrType::StreamInit(e) => write!(f, "Stream Initialisation Error: {}", e),
+            ErrType::Parse(url) => write!(f, "URL Parsing Error: {}", url),
+        }
+    }
+}
+
+// This error is given when the error occurs during the querying
+// In other words, if this error is shown to the user, it means that the media server has not been
+// involved.
+#[derive(Debug)]
+pub struct StreamError {
+    reason: ErrType,
+}
+
+impl Error for StreamError {}
+impl Display for StreamError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.reason.fmt(f)
+    }
+}
+impl StreamError {
+    pub fn stream(e: HttpStreamError<reqwest::Client>) -> StreamError {
+        Self {
+            reason: ErrType::Stream(e),
+        }
+    }
+
+    pub fn stream_init(e: StreamInitializationError<HttpStream<reqwest::Client>>) -> StreamError {
+        Self {
+            reason: ErrType::StreamInit(e),
+        }
+    }
+
+    pub fn parse(e: String) -> StreamError {
+        Self {
+            reason: ErrType::Parse(e),
+        }
+    }
+}

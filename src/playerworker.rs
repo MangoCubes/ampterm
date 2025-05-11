3d2cfc3be23867e91fdcb03f925b15df53ec03a3
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-05-11 13:42:09 +0900

    Add queue

--- src/playerworker.rs
+++ src/playerworker.rs
@@ -1,12 +1,13 @@
 pub mod player;
 pub mod streamerror;
 
+use std::collections::VecDeque;
 use std::sync::Arc;
 
-use color_eyre::{eyre, Result};
+use color_eyre::Result;
 use player::PlayerAction;
-use reqwest::{Client, Url};
-use rodio::{OutputStream, OutputStreamHandle, Sink};
+use reqwest::Url;
+use rodio::{OutputStreamHandle, Sink};
 use stream_download::http::HttpStream;
 use streamerror::StreamError;
 use tokio::sync::mpsc::{self, UnboundedReceiver, UnboundedSender};
@@ -18,11 +19,11 @@ use stream_download::source::SourceStream;
 use stream_download::storage::temp::TempStorageProvider;
 use stream_download::{Settings, StreamDownload};
 
-pub struct PlayState {
+pub struct QueueItem {
     url: String,
 }
 
-impl Clone for PlayState {
+impl Clone for QueueItem {
     fn clone(&self) -> Self {
         Self {
             url: self.url.clone(),
@@ -31,8 +32,8 @@ impl Clone for PlayState {
 }
 
 enum WorkerState {
-    TryPlay(PlayState),
-    Playing(PlayState),
+    TryPlay(QueueItem),
+    Playing(QueueItem),
     Stopped,
 }
 
@@ -44,7 +45,13 @@ pub struct PlayerWorker {
     should_quit: bool,
     config: Config,
     sink: Arc<Sink>,
+    queue: VecDeque<QueueItem>,
 }
+
+// Three actions are available to adding items to the queue
+// 1. Add next
+// 2. Play now
+// 3. Add last
 
 impl PlayerWorker {
     fn continue_stream(&mut self) {
@@ -80,7 +87,6 @@ impl PlayerWorker {
                 PlayerAction::Pause => self.pause_stream(),
                 PlayerAction::TryPlay { url } => {
                     let c = url.clone();
-                    self.state = WorkerState::TryPlay(PlayState { url: c });
                     let sink = self.sink.clone();
                     let action_tx = self.action_tx.clone();
                     let player_tx = self.player_tx.clone();
@@ -98,6 +104,8 @@ impl PlayerWorker {
                             }
                         }
                     });
+
+                    self.state = WorkerState::TryPlay(QueueItem { url: c });
                 }
                 PlayerAction::Continue => self.continue_stream(),
                 PlayerAction::Kill => self.should_quit = true,
@@ -138,6 +146,7 @@ impl PlayerWorker {
             config,
             sink,
             state: WorkerState::Stopped,
+            queue: VecDeque::new(),
         }
     }
     pub fn get_tx(&self) -> UnboundedSender<PlayerAction> {

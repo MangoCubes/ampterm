a6a0678f134ddc39e78191fe88851100f31dcd77
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-11-01 14:04:49 +0000

    Add table display

--- src/components/home/mainscreen/tasks.rs
+++ src/components/home/mainscreen/tasks.rs
@@ -4,20 +4,31 @@ use ratatui::{
     prelude::Rect,
     style::{Modifier, Style, Stylize},
     text::Span,
-    widgets::{Block, Clear},
+    widgets::{Block, Clear, Row, Table},
     Frame,
 };
 
-use crate::components::traits::component::Component;
+use crate::{
+    components::traits::component::Component,
+    queryworker::{
+        highlevelquery::HighLevelQuery,
+        query::{FromQueryWorker, ResponseType, ToQueryWorker},
+    },
+};
 
 pub struct Tasks {
     border: Block<'static>,
+    table: Table<'static>,
 }
 
 impl Tasks {
     pub fn new() -> Self {
         Self {
             border: Self::gen_block(),
+            table: Table::new(
+                [Row::new(vec!["ID", "Task", "Status"])],
+                [Constraint::Max(4), Constraint::Min(1), Constraint::Max(7)],
+            ),
         }
     }
 
@@ -25,6 +36,28 @@ impl Tasks {
         let style = Style::new().white();
         let title = Span::styled("Tasks", Style::default().add_modifier(Modifier::BOLD));
         Block::bordered().title(title).border_style(style)
+    }
+
+    pub fn register_task(&self, task: &ToQueryWorker) {
+        match &task.query {
+            HighLevelQuery::PlayMusicFromURL(media) => todo!(),
+            HighLevelQuery::CheckCredentialValidity => todo!(),
+            HighLevelQuery::SelectPlaylist(get_playlist_params) => todo!(),
+            HighLevelQuery::AddPlaylistToQueue(get_playlist_params) => todo!(),
+            HighLevelQuery::ListPlaylists => todo!(),
+            HighLevelQuery::SetCredential(credential) => {}
+        };
+    }
+
+    pub fn unregister_task(&self, task: &FromQueryWorker) {
+        match &task.res {
+            ResponseType::Ping(ping_response) => todo!(),
+            ResponseType::GetPlaylists(get_playlists_response) => {
+                todo!()
+            }
+            ResponseType::GetPlaylist(get_playlist_response) => todo!(),
+            ResponseType::SetCredential(_) => todo!(),
+        }
     }
 }
 
@@ -36,6 +69,7 @@ impl Component for Tasks {
         let [area] = horizontal.areas(area);
         frame.render_widget(Clear, area);
         frame.render_widget(&self.border, area);
+        frame.render_widget(&self.table, self.border.inner(area));
         Ok(())
     }
 }

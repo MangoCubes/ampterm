12da03492c6c1d2f85e327f80861428764856551
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-11-29 03:04:11 +0000

    Defining HandleKeySeq properly

--- src/components/traits/handlekeyseq.rs
+++ src/components/traits/handlekeyseq.rs
@@ -1,7 +1,13 @@
 use crossterm::event::KeyEvent;
 
-use crate::{action::Action, components::traits::renderable::Renderable};
+use crate::{action::Action, app::Mode, components::traits::renderable::Renderable};
+
+pub enum KeySeqResult {
+    NoMatch,
+    NoActionNeeded,
+    ActionNeeded(Action),
+}
 
 pub trait HandleKeySeq: Renderable {
-    fn handle_key_seq(&mut self, key: Vec<KeyEvent>) -> Option<Action>;
+    fn handle_key_seq(&mut self, mode: &Mode, keyseq: &Vec<KeyEvent>) -> KeySeqResult;
 }

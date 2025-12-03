d4fcf0c938a6615522bfc78ddbd9308b389a1b46
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-12-03 16:12:52 +0000

    Standardise key sequence to string conversion

--- src/config/keybindings.rs
+++ src/config/keybindings.rs
@@ -4,12 +4,20 @@ use crossterm::event::KeyEvent;
 use derive_deref::{Deref, DerefMut};
 use serde::{de::DeserializeOwned, Deserialize, Deserializer};
 
-use crate::{config::keyparser::KeyParser, trace_dbg};
+use crate::{
+    components::traits::handlekeyseq::KeySeqResult, config::keyparser::KeyParser, trace_dbg,
+};
 
 #[derive(Clone, Debug, Deref, DerefMut)]
 pub struct KeyBindings<T: Clone + PartialEq + DeserializeOwned + Debug>(
     pub HashMap<Vec<KeyEvent>, T>,
 );
+
+pub struct KeyBindingHelp {
+    pub keyseq: String,
+    pub hide: bool,
+    pub desc: String,
+}
 
 impl<T: PartialEq + DeserializeOwned + Debug + Clone> Default for KeyBindings<T> {
     fn default() -> Self {
@@ -34,23 +42,25 @@ impl<'de, T: PartialEq + DeserializeOwned + Debug + Clone> Deserialize<'de> for 
 }
 
 impl<T: PartialEq + DeserializeOwned + Debug + Clone> KeyBindings<T> {
+    // pub fn to_help(&self) -> Vec<KeyBindingHelp> {
+    //     self.0
+    //         .iter()
+    //         .map(|(keyseq, action)| KeyBindingHelp {
+    //             keyseq: ,
+    //             hide: todo!(),
+    //             desc: todo!(),
+    //         })
+    //         .collect()
+    // }
     pub fn find_action_str(&self, action: T) -> Option<String> {
-        let strs: Vec<String> = self
-            .find_action(action)?
-            .into_iter()
-            .map(|k| KeyParser::key_event_to_string(&k))
-            .collect();
-        Some(format!("<{}>", strs.join("")))
+        let msg = KeyParser::keyseq_to_string(self.find_action(action)?);
+        Some(format!("<{}>", msg))
     }
 
-    pub fn find_action(&self, action: T) -> Option<Vec<KeyEvent>> {
-        self.0.iter().find_map(|(key, val)| {
-            if action == *val {
-                Some(key.clone())
-            } else {
-                None
-            }
-        })
+    pub fn find_action(&self, action: T) -> Option<&Vec<KeyEvent>> {
+        self.0
+            .iter()
+            .find_map(|(key, val)| if action == *val { Some(key) } else { None })
         // .iter()
         // .find_map(|(key, &val)| if val == value { Some(key) } else { None })
     }

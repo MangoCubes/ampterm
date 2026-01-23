3d02acadca46d42ef3f729a65dfe212bf15cd735
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-01-23 19:46:22 +0000

    Add missing defaults

--- src/config/localkeybinds.rs
+++ src/config/localkeybinds.rs
@@ -12,20 +12,29 @@ use crate::{
 pub struct LocalKeyBinds {
     /// Technically they're tables, but they behave more like a list, which is why they are called
     /// list instead
+    #[serde(default)]
     pub list: KeyBindings<ListAction>,
+    #[serde(default)]
     pub list_visual: KeyBindings<ListAction>,
 
+    #[serde(default)]
     pub help: KeyBindings<HelpAction>,
 
+    #[serde(default)]
     pub playqueue: KeyBindings<PlayQueueAction>,
 
+    #[serde(default)]
     pub lyrics: KeyBindings<LyricsAction>,
 
+    #[serde(default)]
     pub playlistlist: KeyBindings<PlaylistListAction>,
 
+    #[serde(default)]
     pub playlistqueue: KeyBindings<PlaylistQueueAction>,
 
+    #[serde(default)]
     pub popup: KeyBindings<PopupAction>,
 
+    #[serde(default)]
     pub select_playlist_popup: KeyBindings<SelectPlaylistPopupAction>,
 }
